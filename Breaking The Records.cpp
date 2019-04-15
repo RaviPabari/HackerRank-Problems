2445c87078314eee6ff46281fcb596841e5fc72d
Ravi Pabari <46424422+RaviPabari@users.noreply.github.com>
2019-04-15 21:00:58 +0530

    Breaking The Records C++

---
+++ Breaking The Records.cpp
@@ -0,0 +1,33 @@
+//Hackerrank Problem Solving Q: Easy : Breaking The Records Problem 10 points
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	int a[n],i;
+	int max, min,MAX=0,MIN=0;
+	for(i=0;i<n;i++){
+		cin >> a[i];
+	}
+
+	min = a[0];
+	max = a[0];
+
+		for(i=1;i<n;i++){
+		
+		if(a[i]<min){
+			min=a[i];
+			MIN++;		
+		}else if(a[i]>max){
+			max=a[i];
+			MAX++;
+		}
+	}
+	
+	cout << MAX << " "<< MIN;
+	
+	return 0;		
+} 
+
+
+
