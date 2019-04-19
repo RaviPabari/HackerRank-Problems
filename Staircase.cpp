03f0700d1860b8ad92fc0e643e72e61e98bdc46c
Ravi Pabari <46424422+RaviPabari@users.noreply.github.com>
2019-04-19 19:16:11 +0530

    Staircase C++

---
+++ Staircase.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,i,j;
+	cin >> n;
+	for(i=0;i<n;i++){
+		for(j=1;j<n-i;j++){
+			cout << " " ;
+		}
+		for(int k =0; k<=i; k++){
+			cout << "#";
+		}
+		cout << endl;
+	}
+	return 0;
+}
