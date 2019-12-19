4bc911373cc1f741357fba952eeb2184bff100c0
Ravi Pabari <46424422+RaviPabari@users.noreply.github.com>
2019-12-19 13:15:15 +0530

    Extra_Long_Factorials.py

---
+++ Extra_Long_Factorials.py
@@ -0,0 +1,21 @@
+#Hackerrank Problem Solving : Medium : 20 points : Problem --> Extra Long Factorials
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 13:03:41 2019
+
+@author: Ravi
+"""
+#with the help of Dictionary we can solve this easily AF
+#ThankYou Python♥
+def efficient_fact(n,d):
+    if n in d:
+        return d[n]
+    else:
+        ans = n*efficient_fact(n-1,d)
+        d[n]=ans
+        return ans
+   
+d ={0:1,1:1}
+n=int(input())
+print(efficient_fact(n,d))
