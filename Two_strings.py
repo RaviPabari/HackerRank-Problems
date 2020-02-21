fd8b27c3c33617b3ad09c9116fd34240dc8f112a
Ravi Pabari <46424422+RaviPabari@users.noreply.github.com>
2020-02-21 10:36:07 +0530

    Two Strings

---
+++ Two_strings.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 15 01:06:44 2020
+
+@author: Ravi
+"""
+
+def substring(a,b):
+    
+      for i in s1:
+        if i in s2:
+          return "YES"
+                    
+    return "NO"
+            
+    
+
+z = int(input())
+while(z!=0):
+    a = input()
+    b = input()
+    print(substring(a,b))
+    z-=1
